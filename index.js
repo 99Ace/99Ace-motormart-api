f9893df2c8d6db23e8fa64c9303208960e994a6d
99Ace <ace.ang@trentglobal.edu.sg>
2022-09-22 15:54:09 +0000

    init Express and server running

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use((req,res) => {
+    res.send("<h1>Welcome to 99Ace World</h1>")
+})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=> {
+    console.log("Server is running...")
+})
