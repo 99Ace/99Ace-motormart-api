08bb5ba69c8b39d87d201d7166a0772a2360d03f
99Ace <ace.ang@trentglobal.edu.sg>
2022-09-23 00:33:59 +0000

    update index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ app.use((req,res) => {
 })
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=> {
-    console.log("Server is running...")
+    console.log("Server is running...", PORT)
 })
