690cac3c32745b68d14573058fe613b7eecc885c
99Ace <ace.ang@trentglobal.edu.sg>
2022-09-23 09:52:10 +0000

    connected to MONGO Database and read from collection

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require('express');
+const cors = require("cors")
+require("dotenv").config();
+const MongoUtil = require("./MongoUtil");
+const ObjectId = require("mongodb").ObjectId;
+
+// SET UP link to MONGO_URL in .env file
+const mongoUrl = process.env.MONGO_URL;
+let DBNAME = process.env.DBNAME;
+let COLLECTION = process.env.COLLECTION
+
 const app = express();
 
-app.use((req,res) => {
-    res.send("<h1>Welcome to 99Ace World</h1>")
-})
+// ENABLE JSON
+app.use(express.json());
+
+// ENABLE CORS
+app.use(cors());
+console.log(mongoUrl,DBNAME)
+async function main() {
+    
+    // Connect to the database
+    let db = await MongoUtil.connect(mongoUrl, DBNAME);
+    console.log ("Database is connected");
+
+    app.use( async (req,res) => {
+        let data = await db.collection(COLLECTION)
+                    .find()
+                    .toArray()
+        res.send( data )
+    })
+}
+main();
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=> {
     console.log("Server is running...", PORT)
